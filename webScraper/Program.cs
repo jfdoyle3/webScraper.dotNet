0b298765feb3f144eaa13856202f0c3a26028de9
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-04-06 12:04:10 -0400

    have webScraper outputing to console

---
+++ webScraper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace htmlAgilitypack_scrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.yellowpages.com/search?search_terms=bicycles&geo_location_terms=Providence%2C+RI");
+
+            var HeaderNames = doc.DocumentNode
+                .SelectNodes("//a[@class='business-name']").ToList();
+            var PhoneNumbers = doc.DocumentNode
+                .SelectNodes("//div[@class='phones phone primary']").ToList();
+
+            foreach (var companyName in HeaderNames)
+            {
+                Console.WriteLine(companyName.InnerText);
+            }
+            foreach (var phone in PhoneNumbers)
+            {
+                Console.WriteLine(phone.InnerText);
+            }
+            Console.ReadLine();
+        }
+    }
+}
