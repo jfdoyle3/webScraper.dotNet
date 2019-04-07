18df48d0b889fbc9990ae92a083c69d7a401b6a8
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-04-07 09:26:45 -0400

    made a scraper class.

---
+++ webScraper/Scrape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace webScraper
+{
+    class Scrape
+    { 
+        //   HtmlAgilityPack.HtmlDocument doc = doc.Load("file:///D:/repository/webScraper/HTML/Table.html");
+        //   var title = doc.DocumentNode
+        //       .SelectNodes("//a[@class='brand-logo']").ToList();
+        //   foreach(var names in title)
+        //   {
+        //       Console.WriteLine(names.InnerText);
+        //  }
+        public static void Extract()
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.yellowpages.com/search?search_terms=bicycles&geo_location_terms=Providence%2C+RI");
+           
+            var HeaderNames = doc.DocumentNode
+              .SelectNodes("//a[@class='business-name']").ToList();
+            var PhoneNumbers = doc.DocumentNode
+              .SelectNodes("//div[@class='phones phone primary']").ToList();
+
+
+            for (int index = 0; index < HeaderNames.Count; index++)
+
+            {
+                HtmlAgilityPack.HtmlNode companyName = HeaderNames[index];
+                HtmlAgilityPack.HtmlNode phoneNumber = PhoneNumbers[index];
+                Console.Write("{0} | {1}\n", companyName.InnerText, phoneNumber.InnerText);
+            }
+
+
+        }
+    }
+}
