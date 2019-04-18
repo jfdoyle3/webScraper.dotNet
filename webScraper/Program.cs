05aed81a6630fef8762029598e4134bac6a7e4ae
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-04-18 18:52:58 -0400

    added a Try/ Catch. Working on scraping multiple elements

--- webScraper/Program.cs
+++ webScraper/Program.cs
@@ -8,48 +8,61 @@ using HtmlAgilityPack;
 
 namespace WebScraper
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-
-        // Site 1: Proving Ground
-          //String webSite="https://arstechnica.com/gaming/2019/04/terry-gilliams-don-quixote-film-finally-hits-the-big-screen-after-25-years/";
-          // String webSite = "https://arstechnica.com/";
-          //  String webClass = "//p/text()";
-
-
-
-            // Site 2: Yellow Pages: Working - Scrape one Class
-            String webSite = "https://www.yellowpages.com/search?search_terms=bicycles&geo_location_terms=Providence%2C+RI";
-            String webClass = "//a[@class='business-name']";
-
-            // Site 2: Yellow Pages: Scrape 2 Classes
-            //String webSite = "https://www.yellowpages.com/search?search_terms=bicycles&geo_location_terms=Providence%2C+RI";
-            //String webClass = "//a[@class='business-name']";
-            //String webClass1 = "//a[@class='class='phones phone primary']";
+            try
+            {
+                // Site 1: Proving Ground
+                //String webSite="https://arstechnica.com/gaming/2019/04/terry-gilliams-don-quixote-film-finally-hits-the-big-screen-after-25-years/";
+                // String webSite = "https://arstechnica.com/";
+                //  String webClass = "//p/text()";
 
 
-            // https://blog.scrapinghub.com/2016/10/27/an-introduction-to-xpath-with-examples
-            // https://scrapinghub.github.io/xpath-playground/
-            //String webClass = "//a[@class='business-name']"; // <-- XPATH
-            //  the WebClass String uses XPath:
-            //    @ 	    attribute::
-            //    .//    ./descendant-or-self::node()/
-            //    // 	   descendant-or-self::node()/
-            //    *      all child elements of the context node
-            //    @*     all attributes of the context node
-            //    [n] either [position() = n] = index: XPath index start at 1 not 0
-            //    can use math //li[position()%2 = 0] : gets even elements positions
+
+                // Site 2: Yellow Pages: Working - Scrape one Class
+                // String webSite = "https://www.yellowpages.com/search?search_terms=bicycles&geo_location_terms=Providence%2C+RI";
+                // String webClass = "//a[@class='business-name']";
+
+                // Site 3: Yellow Pages: Scrape 2 Classes
+                  String webSite = "https://www.yellowpages.com/search?search_terms=bicycles&geo_location_terms=Providence%2C+RI";
+                //String webClass = "//a[@class='business-name']";
+                // String webClass1 = "//a[@class='class='phones phone primary']";
+                string[] webClass = new string[2] { "//a[@class='business-name']", "//a[@class='class='phones phone primary']" };
+                
+                // Site None: Test Try / Catch erros
+                // String webSite = "http://z.mo";
+                // String webClass = "//a[@class='business-name']";
+
+                // https://blog.scrapinghub.com/2016/10/27/an-introduction-to-xpath-with-examples
+                // https://scrapinghub.github.io/xpath-playground/
+                //String webClass = "//a[@class='business-name']"; // <-- XPATH
+                //  the WebClass String uses XPath:
+                //    @ 	    attribute::
+                //    .//    ./descendant-or-self::node()/
+                //    // 	   descendant-or-self::node()/
+                //    *      all child elements of the context node
+                //    @*     all attributes of the context node
+                //    [n] either [position() = n] = index: XPath index start at 1 not 0
+                //    can use math //li[position()%2 = 0] : gets even elements positions
 
 
-            // Scrape!!
-            Scrape scrape = new Scrape(webSite, webClass);
-            List<HtmlNode> value = scrape.NodesToList();
-            //IEnumerable<HtmlNode> value = scrape.MultiClassScrape();
-            scrape.ToScreen(value);
-            scrape.ToDatabase();
-            scrape.ToFile(value);       
+                // Scrape!!
+                Scrape scrape = new Scrape(webSite, webClass);
+                List<HtmlNode> value = scrape.NodesToList();
+                //IEnumerable<HtmlNode> value = scrape.MultiClassScrape();
+                scrape.ToScreen(value);
+                scrape.ToDatabase();
+                scrape.ToFile(value);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Can't find website:\nnetwork problem\nwebsite not working");
+            }
+            
         }
     }
 }
--- webScraper/Scrape.cs
+++ webScraper/Scrape.cs
@@ -10,43 +10,51 @@ namespace WebScraper
     public class Scrape : Export
     {
         private String WebSite { get; set; }
-        private String WebClass { get; set; }
-        private String WebClass1 { get; set; }
+        private String[] WebClass { get; set; }
+
+        
         private readonly List<HtmlNode> classList = new List<HtmlNode>();
-        public Scrape(String webSite, String webClass)
+        public Scrape(String webSite, String[] webClass)
         {
             this.WebSite = webSite;
-            this.WebClass = webClass;
-           // this.WebClass1 = webClass1;
+            this.WebClass[2] = webClass[2];
+            // this.WebClass1 = webClass1;
 
         }
-        
+
         public List<HtmlNode> NodesToList()
         {
+
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load(WebSite);
-            List<HtmlNode> classList = doc.DocumentNode
-                .SelectNodes(WebClass).ToList();
+            for (int xpath=0; xpath < WebClass.Length; xpath++)
+            {
+               List<HtmlNode> classList = doc.DocumentNode
+               .SelectNodes(WebClass[xpath]).ToList();
+            }
             
-          
-            return classList; 
+
+            return classList;
         }
 
-        public IEnumerable<HtmlNode> MultiClassScrape()
-        {
-            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-            HtmlAgilityPack.HtmlDocument doc = web.Load(WebSite);
-            List<HtmlNode> classList1 = doc.DocumentNode
-                .SelectNodes(WebClass).ToList();
-            List<HtmlNode> classList2 = doc.DocumentNode
-                .SelectNodes(WebClass1).ToList();
-            //foreach to compile list maybe
-            IEnumerable<HtmlNode> completeList = classList1.Concat(classList2);
-            return completeList;
+    
 
-            
-        }
-          
-        
+    //public void  MultiClassScrape()
+    //{
+    //    HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+    //    HtmlAgilityPack.HtmlDocument doc = web.Load(WebSite);
+    //    List<HtmlNode> classList1 = doc.DocumentNode
+    //        .SelectNodes(WebClass).ToList();
+    //    List<HtmlNode> classList2 = doc.DocumentNode
+    //        .SelectNodes(WebClass1).ToList();
+    //    //foreach to compile list maybe
+
+
     }
+
+
+
+      
+
 }
+    
